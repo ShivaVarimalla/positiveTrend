d5969e06938fc7ed70aa2566303ab9444da8c1a6
umakiran9 <umakiran9@github.com>
2020-11-08 18:33:11 +0530

    emergency

---
+++ components/homepage/Emergency.js
@@ -0,0 +1,46 @@
+
+import React from 'react'
+import { View,Button, Text,Modal} from 'react-native'
+
+class Emergency extends React.Component{
+
+    constructor()
+        {
+            super();
+            this.state={
+                show:false
+            }
+        }
+
+    render() {
+        return(
+            <view style={{flex: 1, marginTop:100}}>
+            <Button title="EMERGENCY" onPress={()=>{this.setState({show:true})}}></Button>
+            <Modal
+            transparent={true}
+            visible={this.state.show}
+            >
+           <view style={{BackgroundColor:"#000000aa", flex:1}}>
+            <view style={{BackgroundColor:"#fffffff", margin:50, padding:40, borderRadius:10}}>
+            <Text style={{fontSize: 50}}>Do you need help? </Text>
+            <Button title="NO" onPress={()=>{this.setState({show:false})}}></Button>
+            <Button title="YES" onPress={()=>{this.setState({show:true})}}></Button>
+                <Modal
+                transparent={true}
+                visible={this.state.show}
+                >
+                <view style={{BackgroundColor:"#000000aa", flex:1}}>
+                <view style={{BackgroundColor:"#fffffff", margin:50, padding:40, borderRadius:10}}>
+                <Text style={{fontSize: 50}}>Contact 9152987821 </Text>
+                </Modal>
+                </view>
+                </view>
+            </view>
+            </view>
+            </Modal>
+            </view>
+        )
+    }
+}
+
+export default Emergency;
