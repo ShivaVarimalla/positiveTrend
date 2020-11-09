e9e2d8b78cd2f5f1a5543ac4851f53f975ae45a3
Umakiran9 <67228150+Umakiran9@users.noreply.github.com>
2020-11-09 11:08:14 +0530

    Update Logout.js
    Added logout

--- components/homepage/Logout.js
+++ components/homepage/Logout.js
@@ -1,1 +1,16 @@
+import React from 'react'
+import { View,Button} from 'react-native'
 
+class Logout extends React.Component{
+
+        render(){
+            return(
+                <View style={{BackgroundColor:"#0000FF", justifyContent:"flex-end", alignItems:"flex-end"}}>
+            <Button title="Logout"></Button>
+                </View>
+            )
+        }
+
+}
+
+export default Logout;
