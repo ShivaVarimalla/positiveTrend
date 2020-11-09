0f5928f7eaf231440cd74519e866fc5187a16c7e
Umakiran9 <67228150+Umakiran9@users.noreply.github.com>
2020-11-09 11:08:58 +0530

    Update Home.js
    imported files

--- components/homepage/Home.js
+++ components/homepage/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View,Text,StyleSheet} from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'; 
+import Emergency from './Emergency';
+import Logout from './Logout';
 
 
 const HomeStack = createStackNavigator();
@@ -10,6 +12,7 @@ return (
      <View>
      <Text>Home</Text>
      <Emergency/>
+     <Logout/>
      </View>
 );
 } 
